befd8b1da73cbcb0b0a40dac927277a996ae2178
Jason van Zyl <jason@vanzyl.ca>
2020-04-12 07:42:25 -0400

    Adding functions to parse artifact coordinates into artifact paths

---
+++ maven.bash
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+function mavenCoordinateToArtifactPath() {
+  # Standard format for a Maven coordinate:
+  # <groupId>:<artifactId>[:<extension>[:classifier]]:<version>
+  # $1 = coordinate
+  IFS=':' read -ra coordinateParts <<< "$1"
+  groupId=$(echo ${coordinateParts[0]} | sed 's/\./\//')
+  artifactId=${coordinateParts[1]}
+  if [ ${#coordinateParts[@]} -eq 3 ]; then
+    # <groupId>:<artifactId>:<version>
+    version=${coordinateParts[2]}
+    artifactPath="${groupId}/${artifactId}/${artifactId}-${version}.jar"
+  elif [ ${#coordinateParts[@]} -eq 4 ]; then
+    # <groupId>:<artifactId>:<extension>:<version>
+    version=${coordinateParts[3]}
+    extension=${coordinateParts[2]}
+    artifactPath="${groupId}/${artifactId}/${artifactId}-${version}.${extension}"
+  elif [ ${#coordinateParts[@]} -eq 5 ]; then
+    # <groupId>:<artifactId>:<extension>:<classifier>:<version>
+    version=${coordinateParts[4]}
+    extension=${coordinateParts[2]}
+    classifier=${coordinateParts[3]}
+    artifactPath="${groupId}/${artifactId}/${artifactId}-${version}-${classifier}.${extension}"
+  fi
+  echo $artifactPath
+}
+
+function mavenProjectVersion() {
+  # $1 = pom.xml
+
+  # Strip all the whitespace out of the pom.xml
+  pom=`cat "$1" | tr -d " \t\n\r"`
+
+  # If a parent element exists extract the specified version
+  if grep -q '<parent>' <<< "${pom}"
+  then
+    parentVersion=`echo "${pom}" | \
+      sed -n "s@\(.*\)\(<parent>.*</parent>\)\(.*\)@\2@p" | \
+      sed -n "s@\(.*<version>\)\(.*\)\(</version>\)\(.*\)@\2@p"`
+  fi
+
+  # Strip out all blocks that may contain a <version/> tag which will only
+  # leave the project version if it's present
+  version=`echo "${pom}" | \
+    sed -e 's@<profiles>.*</profiles>@@' | \
+    sed -e 's@<dependencies>.*</dependencies>@@' | \
+    sed -e 's@<build>.*</build>@@' | \
+    sed -e 's@<parent>.*</parent>@@' | \
+    sed -n "s@\(.*<version>\)\(.*\)\(.*</version>\)\(.*\)@\2@p"`
+
+  # If the project version is present use it, otherwise use the parent version
+  [ ! -z "${version}" ] && echo "${version}" || echo "${parentVersion}"
+}
